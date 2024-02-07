ca254a18e18449c7b6fe85ba0033a7f301366fe1
unknown <jose.calero7039@gmail.com>
2024-02-06 22:43:48 -0500

    Se realiza el cambio de string  y datamite en la fecha

--- models/models.py
+++ models/models.py
@@ -7,16 +7,16 @@ class agenda(models.Model):
      _description = 'agenda.agenda'
 
      tecnico = fields.Selection([('Andres Cajas','Andres Cajas'),('Xavier Tituaña','Xavier Tituaña')])
-     kilometraje_inicial = fields.Integer(required=True)
-     kilometraje_final = fields.Integer(required=True)
-     fecha_kilometraje = fields.Char(required=True)
-     valor_combustible_personal= fields.Char(required=True)
-     valor_combustible_laboral= fields.Char(required=True)
+     kilometraje_inicial = fields.Integer(required=True, string="Kilometraje inicial")
+     kilometraje_final = fields.Integer(required=True,string="Kilometraje final")
+     fecha_kilometraje = fields.Datetime(required=True,string="Fecha del kilometraje")
+     valor_combustible_personal= fields.Char(required=True ,string="Valor kilometraje personal")
+     valor_combustible_laboral= fields.Char(required=True,string="Valor kilometraje laboral")
      observacion= fields.Char()
-     imagen_inicial = fields.Binary(required=True)
-     imagen_final = fields.Binary(required=True)
-     kilometraje_total= fields.Integer(required=True)
-     area = fields.Selection([('Soporte técnico','Soporte técnico'),('Instalaciones','Instalaciones'),('Infraestructura','Infraestructura')])
+     imagen_inicial = fields.Binary(required=True ,string="Fotografia kilometraje inicial")
+     imagen_final = fields.Binary(required=True,string="Fotografia kilometraje final")
+     kilometraje_total= fields.Integer(required=True,string="Kilometraje total del dia")
+     area = fields.Selection([('Soporte técnico','Soporte técnico'),('Instalaciones','Instalaciones'),('Infraestructura','Infraestructura')],string="Area encargada")
      @api.onchange('kilometraje_inicial', 'kilometraje_final')
      def onchange_field(self):
         if self.kilometraje_inicial or self.kilometraje_final:
