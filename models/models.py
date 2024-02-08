a5ebb4790a96f9a39f562f76e908be8978a147ea
unknown <jose.calero7039@gmail.com>
2024-02-07 22:20:31 -0500

    Se hace la relación de contactos

--- models/models.py
+++ models/models.py
@@ -6,7 +6,6 @@ class agenda(models.Model):
      _name = 'agenda.agenda'
      _description = 'agenda.agenda'
 
-     tecnico = fields.Selection([('Andres Cajas','Andres Cajas'),('Xavier Tituaña','Xavier Tituaña')])
      kilometraje_inicial = fields.Integer(required=True, string="Kilometraje inicial")
      kilometraje_final = fields.Integer(required=True,string="Kilometraje final")
      fecha_kilometraje = fields.Datetime(required=True,string="Fecha del kilometraje")
@@ -17,6 +16,7 @@ class agenda(models.Model):
      imagen_final = fields.Binary(required=True,string="Fotografia kilometraje final")
      kilometraje_total= fields.Integer(required=True,string="Kilometraje total del dia")
      area = fields.Selection([('Soporte técnico','Soporte técnico'),('Instalaciones','Instalaciones'),('Infraestructura','Infraestructura')],string="Area encargada")
+     contacto =fields.Many2one('res.partner',string="Tecnico")
      @api.onchange('kilometraje_inicial', 'kilometraje_final')
      def onchange_field(self):
         if self.kilometraje_inicial or self.kilometraje_final:
